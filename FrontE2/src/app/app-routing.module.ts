58401ac2785ef5f11dda591512efa69f871d0073
Sergioto275 <sgonzalezm22@fpsanjorge.com>
2024-12-16 10:15:33 +0100

    Hitzordu orria sortu

--- FrontE2/src/app/app-routing.module.ts
+++ FrontE2/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'hitzorduak',
+    loadChildren: () => import('./hitzorduak/hitzorduak.module').then( m => m.HitzorduakPageModule)
+  },
 ];
 
 @NgModule({
