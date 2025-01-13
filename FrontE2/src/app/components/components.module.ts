a437e8b8988246aac4b4236846a7bfa47e50ad27
Alejandro <arivass23@fpsanturtzilh.eus>
2025-01-13 09:10:46 +0100

    mover componentes carpeta

--- FrontE2/src/app/components/components.module.ts
+++ FrontE2/src/app/components/components.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { HomeBotoiakKonponenteaComponent } from './home-botoiak-konponentea/home-botoiak-konponentea.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, HomeBotoiakKonponenteaComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, HomeBotoiakKonponenteaComponent]
 })
 export class ComponentsModule { }
