53c67782e54370f302c30d3b674209daaf9d3a70
Alejandro <arivass23@fpsanturtzilh.eus>
2025-02-11 10:54:02 +0100

    Toasts material

--- FrontE2/src/app/materialak/materialak.page.ts
+++ FrontE2/src/app/materialak/materialak.page.ts
@@ -74,7 +74,7 @@ export class MaterialakPage implements OnInit {
     }
   }
 
-  actualizarMaterialesSeleccionados(material:any, kategoria_id: number) {
+  actualizarMaterialesSeleccionados(material:any) {
     const index = this.materialesSeleccionados.findIndex(p => p.id === material.id);
     if (material.selected && index === -1) {
       this.materialesSeleccionados.push(material);
@@ -192,6 +192,26 @@ export class MaterialakPage implements OnInit {
     });
   }
 
+  toggleMaterialakLortu(){
+    this.mostrarFiltros
+  }
+
+  materialakLortu(){
+    let observableRest: Observable<any> = this.restServer.get<any>('http://localhost:8080/api/material_kategoria');
+    observableRest.subscribe(datuak => {
+      console.log(datuak);
+
+    this.materialak = datuak
+    .filter((categoria:any) => categoria.ezabatzeData === null)
+    .map((categoria:any) => ({
+      ...categoria,
+      materialak: categoria.materialak
+        .filter((material:any) => material.ezabatzeData === null)
+    }));
+    this.filteredMaterialak = this.materialak;
+    });
+  }
+
   materialakLortuAtera() {
     let observableRest: Observable<any> = this.restServer.get<any>('http://localhost:8080/api/material_kategoria');
     observableRest.subscribe(datuak => {
@@ -210,26 +230,6 @@ export class MaterialakPage implements OnInit {
         }));
 
       this.filteredMaterialak = this.materialak;
-    });
-  }
-
-  toggleMaterialakLortu(){
-    this.mostrarFiltros
-  }
-
-  materialakLortu(){
-    let observableRest: Observable<any> = this.restServer.get<any>('http://localhost:8080/api/material_kategoria');
-    observableRest.subscribe(datuak => {
-      console.log(datuak);
-
-    this.materialak = datuak
-    .filter((categoria:any) => categoria.ezabatzeData === null)
-    .map((categoria:any) => ({
-      ...categoria,
-      materialak: categoria.materialak
-        .filter((material:any) => material.ezabatzeData === null)
-    }));
-    this.filteredMaterialak = this.materialak;
     });
   }
 
