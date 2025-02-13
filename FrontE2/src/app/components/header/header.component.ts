156cf413e4d49fde57eac3b4f8f7e78f80a7ebb3
Sergioto275 <sgonzalezm22@fpsanjorge.com>
2025-02-13 13:02:49 +0100

    Azken aldaketak

--- FrontE2/src/app/components/header/header.component.ts
+++ FrontE2/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LoginServiceService } from 'src/app/zerbitzuak/login-service.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,7 @@ import { TranslateService } from '@ngx-translate/core';
 export class HeaderComponent implements OnInit {
   public appPages: any[] = [];
 
-  constructor(private translate: TranslateService) {}
+  constructor(private translate: TranslateService, private loginService: LoginServiceService) {}
 
   ngOnInit() {
     this.loadTranslations();
@@ -17,22 +18,28 @@ export class HeaderComponent implements OnInit {
 
   public loadTranslations() {
     this.translate.get([
-      'menu.home', 'menu.hitzorduak', 'menu.txandak','menu.grafikoak', 'menu.materialak', 
-      'menu.produktuak', 'menu.historiala', 'menu.ikasleak', 'menu.zerbitzuak'
+      'menu.home', 'menu.hitzorduak', 'menu.txandak', 'menu.grafikoak', 
+      'menu.materialak', 'menu.produktuak', 'menu.historiala', 
+      'menu.ikasleak', 'menu.zerbitzuak'
     ]).subscribe(translations => {
-      this.appPages = [
+
+      // Opciones para todos los usuarios
+      const commonPages = [
         { title: translations['menu.home'], url: '/home', icon: 'home' },
         { title: translations['menu.hitzorduak'], url: '/hitzorduak', icon: 'calendar-number' },
         { title: translations['menu.txandak'], url: '/txandak', icon: 'repeat' },
         { title: translations['menu.materialak'], url: '/materialak', icon: 'cut' },
         { title: translations['menu.produktuak'], url: '/produktuak', icon: 'color-fill' },
-        { title: translations['menu.zerbitzuak'], url: '/tratamenduak', icon: 'color-palette' },
-        { title: translations['menu.ikasleak'], url: '/ikasleak', icon: 'people' },
         { title: translations['menu.historiala'], url: '/historiala', icon: 'document-text' },
-        { title: translations['menu.grafikoak'], url: '/grafikoak', icon: 'stats-chart' },
-
       ];
+
+      const adminPages = [
+        ...commonPages,
+        { title: translations['menu.grafikoak'], url: '/grafikoak', icon: 'stats-chart' },
+        { title: translations['menu.ikasleak'], url: '/ikasleak', icon: 'people' },
+        { title: translations['menu.zerbitzuak'], url: '/tratamenduak', icon: 'color-palette' }
+      ];
+      this.appPages = this.loginService.isAlumno() ? commonPages : adminPages;
     });
   }
-  
 }
